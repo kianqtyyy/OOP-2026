5165c0c8e04e914159b441619e7395b10321eb03
kianqtyyy <kiandaveigh.estigoy@lorma.edu>
2026-01-28 12:27:19 +0800

    nyark

---
+++ activity2/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+   int studentId;
+   String firstName;
+   String middleName;
+   String lastName;
+   String gender;
+   String email;
+
+   public int totalStudent = 0;
+
+   public Student(String firstName, String middleName, String lastName, String gender, String email){
+      this.firstName = firstName;
+      this.middleName = middleName;
+      this.lastName = lastName;
+      this.gender = gender;
+      this.email = email;
+      
+
+   }
+
+}
